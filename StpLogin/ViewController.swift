6224bdb56c87c50b605f89771de64e8f33581a3b
Rafy Zhao <rafyzhao@nes-macbook-air.local>
2016-11-01 09:34:43 -0700

    Initial Commit

---
+++ StpLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StpLogin
+//
+//  Created by Rafy Zhao on 2016-11-01.
+//  Copyright © 2016 Rafy Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
